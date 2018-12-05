3d4097fde79e8b3a1d1314c762f5ecf059db8e70
Boyun Zou <zouboyun@Boyuns-MacBook-Pro.local>
2018-12-05 14:17:48 -0800

    set up eslint

--- challenge_1/webpack.config.js
+++ challenge_1/webpack.config.js
@@ -25,6 +25,11 @@ module.exports = {
         },
       },
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader'],
+      },
+      {
         test: /\.css$/,
         loader: 'style-loader',
       },
