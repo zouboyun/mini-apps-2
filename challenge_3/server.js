6e951e108555e639fbfc3a73f0e277253e847e0a
Boyun Zou <zouboyun@Boyuns-MacBook-Pro.local>
2018-12-07 12:16:42 -0800

    started express server

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+ 
+// parse application/json
+app.use(bodyParser.json());
 
+// static file serving
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, () => {
+  console.log(`app is listening on port ${port}`);
+});
